31a1bf6e99c5bf32be0f646db0d756c4af12c5ce
Arthur Lockman <hello@rthr.me>
2016-11-07 16:57:50 -0500

    Added basic HTML.

--- server.js
+++ server.js
@@ -60,16 +60,32 @@ function sendIndex(res) {
   html = html + '<html>'
 
   html = html + '<head>'
-  // You could add a CSS and/or js call here...
+  html = html + '<meta charset="utf-8">'
+  html = html + '<meta http-equiv="X-UA-Compatible" content="IE=edge">'
+  html = html + '<meta name="viewport" content="width=device-width, initial-scale=1">'
+  html = html + '<title>Movie Search</title>'
+  html = html + '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">'
+  html = html + '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">'
+  html = html + '<link rel="stylesheet" href="css/style.css" type="text/css">'
+  html = html + '<!--[if lt IE 9]>'
+  html = html + '<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>'
+  html = html + '<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>'
+  html = html + '<![endif]-->'
   html = html + '</head>'
 
   html = html + '<body>'
+  html = html + '<div class="navbar navbar-inverse navbar-fixed-top">'
+  html = html + '<div class="navbar-header theme-showcase" role="main">'
+  html = html + '<a class="navbar-brand" href="#">CS 4241 Movie Search</a>'
+  html = html + '</div>'
+  html = html + '</div>'
+  html = html + '<div class="container">' //Starting bootstrap content
   html = html + '<h1>Movie Search!</h1>'
 
   // Here's where we build the form YOU HAVE STUFF TO CHANGE HERE
   html = html + '<form action="TODO" method="TODO">'
-  html = html + '<input type="TODO" name="TODO" />'
-  html = html + '<button type="TODO">Search</button>'
+  html = html + '<input type="TODO" name="TODO" class="form-control"/>'
+  html = html + '<button type="TODO" class="btn btn-default">Search</button>'
   html = html + '</form>'
 
   html = html + '<ul>'
@@ -82,7 +98,9 @@ function sendIndex(res) {
   // For a challenge, try rewriting this function to take the filtered movies list as a parameter, to avoid changing to a page that lists only movies.
   html = html + movies.map(function(d) { return '<li>'+d+'</li>' }).join(' ')
   html = html + '</ul>'
-
+  html = html + '</div>' //Ending bootstrap content
+  html = html + '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>'
+  html = html + '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>'
   html = html + '</body>'
   html = html + '</html>'
   
